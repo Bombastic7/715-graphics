2d8053695cc43b36b40401487c3d4e1eb3eb4baa
PriyankitSingh <psin778@aucklanduni.ac.nz>
2017-09-20 13:20:09 +1200

    region growing to compute surfaces

--- testing/read_pcl/read_pcl.cpp
+++ testing/read_pcl/read_pcl.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
-
-
+#include "RegionGrowing.h"
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing.h>
+#include <vector>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 
 int
 main (int argc, char** argv)
 {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  
+  std::cout << "testing";
   if(argc == 1)
 	return 1;
 
@@ -23,7 +31,61 @@ main (int argc, char** argv)
             << cloud->width * cloud->height
             << std::endl;
 
+  std::cout << "computing normals\n";
+  // compute normals
+  pcl::search::Search<pcl::PointXYZ>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZ> >(new pcl::search::KdTree<pcl::PointXYZ>);
+  pcl::PointCloud <pcl::Normal>::Ptr normals(new pcl::PointCloud <pcl::Normal>);
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+  normal_estimator.setSearchMethod(tree);
+  normal_estimator.setInputCloud(cloud);
+  normal_estimator.setKSearch(50);
+  normal_estimator.compute(*normals);
 
+  std::cout << "filtering indices\n";
+  // filtering indices (probably don't need this)
+  pcl::IndicesPtr indices(new std::vector <int>);
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud(cloud);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(0.0, 1.0);
+  pass.filter(*indices);
+
+  std::cout << "region growing\n";
+  // region growing algorithm
+  pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+  reg.setMinClusterSize(50);
+  reg.setMaxClusterSize(1000000);
+  reg.setSearchMethod(tree);
+  reg.setNumberOfNeighbours(30);
+  reg.setInputCloud(cloud);
+  //reg.setIndices (indices);
+  reg.setInputNormals(normals);
+  reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
+  reg.setCurvatureThreshold(1.0);
+
+  std::vector <pcl::PointIndices> clusters;
+  reg.extract(clusters);
+
+  std::cout << "Number of clusters is equal to " << clusters.size() << std::endl;
+  std::cout << "First cluster has " << clusters[0].indices.size() << " points." << endl;
+  std::cout << "These are the indices of the points of the initial" <<
+	  std::endl << "cloud that belong to the first cluster:" << std::endl;
+  int counter = 0;
+  while (counter < clusters[0].indices.size())
+  {
+	  std::cout << clusters[0].indices[counter] << ", ";
+	  counter++;
+	  if (counter % 10 == 0)
+		  std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud();
+  pcl::visualization::CloudViewer viewer("Cluster viewer");
+  viewer.showCloud(colored_cloud);
+  while (!viewer.wasStopped())
+  {
+  }
 
   return (0);
 }
