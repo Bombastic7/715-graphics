ff0c94dc289535f0beac15301688334c6a975782
matthew <matthew@debian.private>
2017-09-28 21:51:15 +1300

    Colour scale functions in common.h, two new test_models

--- components/common/include/common.h
+++ components/common/include/common.h
@@ -1,8 +1,99 @@
 #include <string>
-
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
 
 inline bool ends_with(std::string const & value, std::string const & ending)
 {
   if (ending.size() > value.size()) return false;
   return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
+
+
+template<typename PointT>
+int load_pcd_ply(std::string const& f, typename pcl::PointCloud<PointT>::Ptr cloud) {
+  int res;
+  if(ends_with(f, ".pcd") && (res = pcl::io::loadPCDFile(f, *cloud)) == 0) {}
+  else if(ends_with(f, ".ply") && (res = pcl::io::loadPLYFile(f, *cloud)) == 0) {}
+  else {
+	  PCL_ERROR("Bad input file. %s\n", f);
+      return -1;
+  }
+  return 0;
+}
+
+
+enum {
+  COLOR_MAP_RAINBOW, COLOR_MAP_COOLWARM
+};
+
+
+template<typename T, typename U>
+void rainbow_scale_value(T val, U& r, U& g, U& b) {
+  if(!isfinite(val)) {
+    r = g = b = 255;
+    return;
+  }
+  
+  int h = int(val * 256 * 6);
+  int x = h % 0x100;
+
+  r = 0, g = 0, b = 0;
+  switch (h / 256)
+  {
+    case 0: r = 255; g = x;       break;
+    case 1: g = 255; r = 255 - x; break;
+    case 2: g = 255; b = x;       break;
+    case 3: b = 255; g = 255 - x; break;
+    case 4: b = 255; r = x;       break;
+    case 5: r = 255; b = 255 - x; break;
+  }
+}
+
+
+//A really quick color map. Simple linear interpolation between (85,72,193), (242,242,242), (177,1,39) for 0, 0.5, 1, respectively.
+//If val is not finite, colour is off map.
+template<typename T, typename U>
+void coolwarm_scale_value(T val, U& r, U& g, U& b) {
+  if(!isfinite(val)) {
+    r = b = 255;
+    g = 0;
+    return;
+  }
+  
+  if(val < 0.5) {
+    r = 85 + 157 * val;
+    g = 72 + 170 * val;
+    b = 193 + 49 * val;
+  }
+  else {
+    r = 177 + 65 * (1 - val);
+    g = 1 + 241 * (1 - val);
+    b = 39 + 203 * (1-val);
+  }
+}
+
+template<typename U, typename PointT>
+void make_rgb_scale(std::vector<U>& vals, typename pcl::PointCloud<PointT>::Ptr cloud_rgb, int map_type) {
+
+  if(vals.size() == 0)
+    return;
+  
+  U val_min, val_max;
+  val_min = val_max = vals[0];
+  
+  for(auto it = vals.begin(); it != vals.end(); ++it) {
+    U v = *it;
+    if(v < val_min)
+      val_min = v;
+    if(v > val_max)
+      val_max = v;
+  }
+  
+  for(int i=0; i<vals.size(); i++) {
+    if(map_type == COLOR_MAP_RAINBOW)
+      rainbow_scale_value((vals[i] - val_min) / (val_max - val_min), cloud_rgb->points[i].r, cloud_rgb->points[i].g, cloud_rgb->points[i].b);
+    if(map_type == COLOR_MAP_COOLWARM)
+      coolwarm_scale_value((vals[i] - val_min) / (val_max - val_min), cloud_rgb->points[i].r, cloud_rgb->points[i].g, cloud_rgb->points[i].b);
+  }
 }
--- components/normal_estimation/normal_estimation.cpp
+++ components/normal_estimation/normal_estimation.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
 #include <boost/thread/thread.hpp>
+#include <pcl/common/eigen.h>
+#include <pcl/ModelCoefficients.h>
 #include <pcl/console/parse.h>
+#include <pcl/filters/extract_indices.h>
 #include <pcl/features/normal_3d.h>
-#include <pcl/io/ply_io.h>
-#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
 #include <pcl/search/search.h>
 #include <pcl/search/kdtree.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
 #include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/visualization/point_cloud_color_handlers.h>
-#include <pcl/visualization/point_cloud_geometry_handlers.h>
+//#include <pcl/visualization/point_cloud_color_handlers.h>
+//#include <pcl/visualization/point_cloud_geometry_handlers.h>
 
 #include "common.h"
 
 
 std::string g_infile, g_outfile;
+float g_norm_est_radius = 0.03;
 
 
 void print_help(int argc, char* argv[]) {
-	std::cout << "USAGE: " << argv[0] << "[-o outfile] [-i infile]" << std::endl;
+	std::cout << "USAGE: " << argv[0] << "[-o outfile] [-i infile] [-r radius]" << std::endl;
 	std::cout << "Input file either PLY or PCD format. Output file is PLY." << std::endl;
 }
 
@@ -29,6 +37,7 @@ bool parse_input(int argc, char* argv[]) {
 
   pcl::console::parse_argument(argc, argv, "-i", g_infile);
   pcl::console::parse_argument(argc, argv, "-o", g_outfile);
+  pcl::console::parse_argument(argc, argv, "-r", g_norm_est_radius);
   
   
   if(g_infile == "") {
@@ -40,17 +49,150 @@ bool parse_input(int argc, char* argv[]) {
 }
 
 
-template<typename PointT>
-int load_file(std::string const& f, typename pcl::PointCloud<PointT>::Ptr cloud) {
-  int res;
-  if(ends_with(f, ".pcd") && (res = pcl::io::loadPCDFile(f, *cloud)) == 0) {}
-  else if(ends_with(f, ".ply") && (res = pcl::io::loadPLYFile(f, *cloud)) == 0) {}
-  else {
-	  PCL_ERROR("Bad input file. %i\n", res);
-      return -1;
+
+
+template<typename PointP, typename PointN>
+int compute_normals(  typename pcl::PointCloud<PointP>::Ptr cloud, 
+                      typename pcl::PointCloud<PointN>::Ptr cloud_normals, 
+                      float rad) {
+  pcl::NormalEstimation<PointP, PointN> normal_est;
+  normal_est.setInputCloud (cloud);
+
+  typename pcl::search::KdTree<PointP>::Ptr tree (new pcl::search::KdTree<PointP> ());
+  normal_est.setSearchMethod (tree);
+
+  normal_est.setRadiusSearch (rad);
+
+  normal_est.compute (*cloud_normals);
+	return 0;
+}
+
+
+
+template<typename PointType>
+double
+computeCloudResolution (typename pcl::PointCloud<PointType>::Ptr &cloud)
+{
+  double res = 0.0;
+  int n_points = 0;
+  int nres;
+  std::vector<int> indices (2);
+  std::vector<float> sqr_distances (2);
+  pcl::search::KdTree<PointType> tree;
+  tree.setInputCloud (cloud);
+
+  for (size_t i = 0; i < cloud->size (); ++i)
+  {
+    if (! pcl_isfinite ((*cloud)[i].x))
+    {
+      continue;
+    }
+    //Considering the second neighbor since the first is the point itself.
+    nres = tree.nearestKSearch (i, 2, indices, sqr_distances);
+    if (nres == 2)
+    {
+      res += sqrt (sqr_distances[1]);
+      ++n_points;
+    }
   }
+  if (n_points != 0)
+  {
+    res /= n_points;
+  }
+  return res;
+}
+
+
+float g_dist_threshold = 0.01;
+
+
+
+
+template<typename PointT>
+int compute_position_deviance(typename pcl::PointCloud<PointT>::Ptr cloud, std::vector<float>& deviance, float dist_threshold) {
+  //Position deviance
+  //Compute best plane model fit using RANSAC.
+  //Options: model distance threshold.
+  
+  pcl::SACSegmentation<PointT> seg;
+  pcl::ModelCoefficients::Ptr coeffs (new pcl::ModelCoefficients);
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  
+  seg.setOptimizeCoefficients(true);
+  seg.setModelType(pcl::SACMODEL_PLANE);
+  seg.setMethodType(pcl::SAC_RANSAC);
+  seg.setDistanceThreshold(dist_threshold);
+  
+  seg.setInputCloud(cloud);
+  seg.segment(*inliers, *coeffs);
+  
+  //... compute deviance of each point wrt computed plane.
+}
+
+
+
+template<typename PointT>
+int compute_normal_deviance(typename pcl::PointCloud<PointT>::Ptr cloud, std::vector<float>& deviance, float rad1, float rad2) {
+
+  pcl::PointCloud<pcl::Normal>::Ptr normals_1(new pcl::PointCloud<pcl::Normal>), normals_2(new pcl::PointCloud<pcl::Normal>);
+  
+  compute_normals<PointT, pcl::Normal>(cloud, normals_1, rad1);
+  compute_normals<PointT, pcl::Normal>(cloud, normals_2, rad2);
+
+  deviance.resize(cloud->points.size());
+  
+  for(int i=0; i<cloud->points.size(); i++) {
+    Eigen::Vector3f n1 = normals_1->points[i].getNormalVector3fMap();
+    Eigen::Vector3f n2 = normals_2->points[i].getNormalVector3fMap();
+    
+    deviance[i] = n1.dot(n2);
+  }
+  
   return 0;
 }
+
+
+//PointT expected to have computed normal/curvature component, xyz component.
+template<typename PointT>
+int compute_interest(typename pcl::PointCloud<PointT>::Ptr cloud, std::vector<float>& interest) {
+  //interest = curvature + curvature deviance + position deviance + normal deviance
+  
+
+  return 0;
+  
+}
+
+
+template<typename PointT>
+int extract_interest_clusters(  typename pcl::PointCloud<PointT>::Ptr cloud, 
+                                std::vector<float>& interest_val, 
+                                float interest_th,
+                                float cluster_tolerance,
+                                std::vector<pcl::PointIndices>& cluster_indices)
+{
+	
+  pcl::IndicesPtr kept_indices = pcl::IndicesPtr(new std::vector<int>);
+  
+  for(int i=0; i<cloud->size(); i++)
+    if(interest_val[i] >= interest_th)
+      kept_indices->push_back(i);
+  
+  pcl::search::KdTree<PointT> tree;
+  tree.setInputCloud (cloud);
+  
+  pcl::EuclideanClusterExtraction<PointT> ec;
+  
+  ec.setSearchMethod(tree);
+  ec.setMinClusterSize(50);
+  ec.setMaxClusterSize(cloud->size());
+  ec.setClusterTolerance(0.3);
+  ec.setInputCloud(cloud);
+  ec.setIndices(kept_indices);
+  ec.extract(cluster_indices);
+
+}
+
+
 
 
 int main(int argc, char* argv[]) {
@@ -63,80 +205,38 @@ int main(int argc, char* argv[]) {
     return 1;
   
   pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-  //pcl::PointCloud<pcl::PointXYZ>::Ptr cloudxyz (new pcl::PointCloud<pcl::PointXYZ>);
   
-  if(load_file<pcl::PointXYZRGBNormal>(g_infile, cloud) == -1)
-	return -1;
+  if(load_pcd_ply<pcl::PointXYZRGBNormal>(g_infile, cloud) == -1)
+    return -1;
 
-/*
-  cloud->points.resize(cloudxyz->points.size());
-  cloud->width = cloudxyz->points.size();
-  cloud->height = 1;
-  cloud->is_dense = true;
-  
-  for(int i=0; i<cloudxyz->points.size(); i++) {
-	  cloud->points[i].x = cloudxyz->points[i].x;
-	  cloud->points[i].y = cloudxyz->points[i].y;
-	  cloud->points[i].z = cloudxyz->points[i].z;
-  }
-*/
 
-  pcl::NormalEstimation<pcl::PointXYZRGBNormal, pcl::PointXYZRGBNormal> normal_est;
-  normal_est.setInputCloud (cloud);
+  std::vector<float> ndeviance;
+  
+  compute_normal_deviance<pcl::PointXYZRGBNormal>(cloud, ndeviance, 0.03, 0.3);
+  
+  //make_rgb_scale<float, pcl::PointXYZRGBNormal>(ndeviance, cloud, COLOR_MAP_COOLWARM);
 
-  // Create an empty kdtree representation, and pass it to the normal estimation object.
-  // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-  pcl::search::KdTree<pcl::PointXYZRGBNormal>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBNormal> ());
-  normal_est.setSearchMethod (tree);
-
-  normal_est.setRadiusSearch (0.03);
-
-  normal_est.compute (*cloud);
+  //std::vector<pcl::PointIndices> cluster_indices;
+  //extract_interest_clusters(cloud, ndeviance, 0.2
   
+  pcl::IndicesPtr kept_indices = pcl::IndicesPtr(new std::vector<int>);
   
-  
-  float curv_min, curv_max;
-  curv_min = curv_max = cloud->points[0].curvature;
-  
-  for(auto it = cloud->points.begin(); it != cloud->points.end(); ++it) {
-	  if(it->curvature < curv_min)
-		curv_min = it->curvature;
-	  if(it->curvature > curv_max)
-	    curv_max = it->curvature;
+  for(int i=0; i<cloud->size(); i++) {
+    if(ndeviance[i] >= 0.2)
+      kept_indices->push_back(i);
   }
   
-  for(auto it = cloud->points.begin(); it != cloud->points.end(); ++it) {
-    int h = int((curv_max - it->curvature) / (curv_max - curv_min) * 256 * 6);
-    int x = h % 0x100;
-
-    int r = 0, g = 0, b = 0;
-    switch (h / 256)
-    {
-      case 0: r = 255; g = x;       break;
-      case 1: g = 255; r = 255 - x; break;
-      case 2: g = 255; b = x;       break;
-      case 3: b = 255; g = 255 - x; break;
-      case 4: b = 255; r = x;       break;
-      case 5: r = 255; b = 255 - x; break;
-    }
-    it->r = r;
-    it->g = g;
-    it->b = b;
-  }
+  pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+  pcl::ExtractIndices<pcl::PointXYZRGBNormal> ei_filter(true);
+  ei_filter.setInputCloud(cloud);
+  ei_filter.setIndices(kept_indices);
+  ei_filter.filter(*cloud_filtered);
   
-  
-
   pcl::visualization::PCLVisualizer viewer(std::string("PCLVisualizer: ") + g_infile);
-  
-  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBNormal> rgb_handler(cloud);
-  pcl::visualization::PointCloudGeometryHandlerXYZ<pcl::PointXYZRGBNormal> xyz_handler(cloud);
-  
-  //viewer.addPointCloudNormals<pcl::PointNormal>(cloud);
-  viewer.addPointCloud<pcl::PointXYZRGBNormal>(cloud, rgb_handler, xyz_handler);
-  //viewer.addPointCloudNormals<pcl::PointXYZRGBNormal>(cloud, 100, 0.02f, "normals");
+  viewer.addPointCloud<pcl::PointXYZRGBNormal>(cloud_filtered);
   
   while (!viewer.wasStopped()) {
-	 viewer.spinOnce (100);
-     boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    viewer.spinOnce (100);
+    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
    }
 }
